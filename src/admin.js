95d97fe1e576c611751768c8e7a1f4cb5fa7f520
wdqyj-sx <wenyeqv@163.com>
2021-01-13 23:05:10 +0800

    ui

--- src/admin.js
+++ src/admin.js
@@ -3,16 +3,17 @@ import Header from "./component/Header";
 import Footer from "./component/Footer";
 import Navlist from "./component/Navlist";
 import {Col,Row} from "antd";
+import "./admin.less";
 
 export default class Admin extends React.Component {
     render(){
         return (
             <div>
                 <Row>
-                    <Col span={4}>
+                    <Col span={4} className="left">
                         <Navlist></Navlist>
                     </Col>
-                    <Col>
+                    <Col span={20} className="right">
                         <Header></Header>
                         <Footer></Footer>
                     </Col>
--- src/component/Navlist/index.js
+++ src/component/Navlist/index.js
@@ -1,9 +1,29 @@
 import React from "react";
+import {Menu} from "antd";
+import "./index.less";
+import menuConfig from "../../../config/menuConfig";
 
+const {SubMenu} = Menu;
 export default class Navlist extends React.Component{
+    state = {
+        list:''
+    }
+    mapList = (navlist)=>{
+        navlist.map((item)=>{
+            if(item.children){
+                this.mapList(item.children)
+            }
+            return 
+        })
+    }
+    componentWillMount() {
+       
+    }
     render(){
         return (
-            <div>这是左侧</div>
+            <div className="logo">
+                <h1>简单管理系统</h1>
+            </div>
         )
     }
 }
